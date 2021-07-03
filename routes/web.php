71c674ac370d9e6217d1fb05a4c26c9cba453bec
clarizze <jian.clarizze@gmail.com>
2021-07-03 21:39:38 +0800

    Setup:Database&Normal user routing\controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('user')->name('user.')->group(function(){
+
+    Route::middleware(['guest'])->group(function(){
+        Route::view('/login','dashboard.user.login')->name('login');
+        Route::view('/register','dashboard.user.register')->name('register');
+        
+    });
+
+    Route::middleware(['auth'])->group(function(){
+        Route::view('/home','dashboard.user.home')->name('home');
+        
+    });
+
 });
