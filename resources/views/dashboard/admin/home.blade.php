1e5b87e81d0acd9c697b97d8f77effa3d93f51f4
clarizze <jian.clarizze@gmail.com>
2021-10-17 22:27:09 +0800

    Partials for Admin Panel

--- resources/views/dashboard/admin/home.blade.php
+++ resources/views/dashboard/admin/home.blade.php
@@ -18,12 +18,16 @@
 </head>
 <body style="font-family: 'Nunito', sans-serif">
     <!-- Image and text -->
-    <nav class="navbar navbar-light" style="background-color:#FBD848">
-    <a class="navbar-brand" href="#" >
-        <img src="/assets/Logo.png" width="30" height="30" style="border-radius:100%;border: 2px solid #FD6300 " class="d-inline-block align-top" alt="AMSAI Logo">
-        AMSAI Student Information System
-    </a>
+
+    <!-- SIDE bar -->
+
+    <!-- NAV BAR -->
+
+    <nav>
+        @include('partials.admin.navbar')
     </nav>
+
+    <section>
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 offset-md-3" style="margin-top: 45px">
@@ -55,6 +59,13 @@
             </div>
         </div>
     </div>
+    </section>
+    
+    
+<!--    FOOTER     -->
+    <footer class="row">
+        @include('partials.admin.footer')
+    </footer>
     
 </body>
 </html>
---
+++ resources/views/partials/admin/navbar.blade.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-light" style="background-color:#FBD848">
+    <a class="navbar-brand" href="#" >
+        <img src="/assets/Logo.png" width="30" height="30" style="border-radius:100%;border: 2px solid #FD6300 " class="d-inline-block align-top" alt="AMSAI Logo">
+        AMSAI Student Information System
+    </a>
+    </nav>
