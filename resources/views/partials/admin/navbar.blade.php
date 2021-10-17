c1ae0a82c2fd051d8d0fa6b22baf56f75dca62ba
clarizze <jian.clarizze@gmail.com>
2021-10-18 01:11:02 +0800

    Position Admin Sidebar in navbar.blade

--- resources/views/partials/admin/navbar.blade.php
+++ resources/views/partials/admin/navbar.blade.php
@@ -1,7 +1,34 @@
+
+<link rel="stylesheet" href="{{ URL::asset('css/partials/sidebar.css') }}" />
+
+<div id="mySidebar" class="sidebar">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+
 <nav class="navbar navbar-light" style="background-color:#FBD848">
+<button class="openbtn" onclick="openNav()">☰</button>  
+
     <a class="navbar-brand" href="#" >
         <img src="/assets/Logo.png" width="30" height="30" style="border-radius:100%;border: 2px solid #FD6300 " class="d-inline-block align-top" alt="AMSAI Logo">
         AMSAI Student Information System
     </a>
-    </nav>
+</nav>
+
+<script>
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+
+
 
