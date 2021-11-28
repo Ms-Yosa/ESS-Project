fae50ab539c5c371c373b4252d97b266b72868cd
Ms-Yosa <jian.clarizze@gmail.com>
2021-11-28 14:03:04 +0800

    Rename birthyear

--- resources/views/admin/student-management/register.blade.php
+++ resources/views/admin/student-management/register.blade.php
@@ -90,8 +90,8 @@
                                             
                                                 <option value="2019" >2019</option>
                                                 <option value="2018" >2018</option>
-                                                <option value="2021" >2017</option>
-                                                <option value="2017" >2016</option>
+                                                <option value="2017" >2017</option>
+                                                <option value="2016" >2016</option>
                                                 <option value="2015" >2015</option>
                                                 <option value="2014" >2014</option>
                                                 <option value="2013" >2013</option>
