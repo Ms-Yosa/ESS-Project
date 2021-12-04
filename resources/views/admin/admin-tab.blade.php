cdb4b2c343a1e655dff93907c67216eab9937c77
Ms-Yosa <jian.clarizze@gmail.com>
2021-12-04 18:48:33 +0800

    Admin tab header

--- resources/views/admin/admin-tab.blade.php
+++ resources/views/admin/admin-tab.blade.php
@@ -7,7 +7,7 @@
             <div class="row page-titles mx-0">
                 <div class="col-sm-6 p-md-0">
                     <div class="welcome-text">
-                        <h4>Faculties Tab</h4>
+                        <h4>Admin Tab</h4>
                     </div>
                 </div>
                 <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
