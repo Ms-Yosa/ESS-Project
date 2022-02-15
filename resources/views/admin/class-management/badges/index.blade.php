e67a544b9f46b4687f14b860e3c7304e9911354d
Ms-Yosa <jian.clarizze@gmail.com>
2022-02-16 00:30:35 +0800

    badges clean up

--- resources/views/admin/class-management/badges/index.blade.php
+++ resources/views/admin/class-management/badges/index.blade.php
@@ -7,13 +7,14 @@
             <div class="row page-titles mx-0">
                 <div class="col-sm-6 p-md-0">
                     <div class="welcome-text">
-                        <h4>Classes Tab</h4>
+                        <h4>Badges Tab</h4>
                     </div>
                 </div>
                 <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Home</a></li>
                         <li class="breadcrumb-item active"><a href="javascript:void(0);">Class Management</a></li>
+                        <li class="breadcrumb-item active"><a href="#">Badge</a></li>
                     </ol>
                 </div>
             </div>
